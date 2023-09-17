85393cd7e7b15167a0c5887c222a35b0a0fdf28f
Jamil Daoud <lamamasri@dyn-172-20-98-186.qatar.cmu.local>
2023-09-17 12:52:49 +0300

    Deleted the function and the event listener in this file as I found out we do not need it and its presence will cause future problems, so I deleted it.

--- public/src/client/compose.js
+++ public/src/client/compose.js
@@ -1,22 +1,5 @@
 'use strict';
 
-const myButton = document.getElementById('myButton');
-const postData = {
-  user: {}
-};
-
-myButton.addEventListener('click', () => {
-  if (data.isAnonymous) {
-    Object.assign(postData.user, {
-      displayname: 'Anonymous',
-      userslug: '',
-      status: 'offline',
-      picture: '/assets/uploads/profile/anonymous.png'
-    });
-  }
-});
-
-
 define('forum/compose', ['hooks'], function (hooks) {
     const Compose = {};
 
