173a8045cf94d204cede5a8c2f5f6c8979971b59
Jamil Daoud <lamamasri@dyn-172-20-98-186.qatar.cmu.local>
2023-09-17 12:46:50 +0300

    Added a function for handling the offline feature in our issue, because we need the anonymous person to always be shown offline. After it, I added an event listener that works on click of the toggle button we have

--- public/src/client/compose.js
+++ public/src/client/compose.js
@@ -1,5 +1,17 @@
 'use strict';
 
+function handleButtonClick() {
+    if (data.isAnonymous) {
+        postData.user.displayname = "Anonymous";
+        postData.user.userslug = "";
+        postData.user.status = 'offline';
+        postData.user.picture = '/assets/uploads/profile/anonymous.png';
+    } 
+}
+
+const myButton = document.getElementById('myButton');
+
+myButton.addEventListener('click', handleButtonClick);
 
 define('forum/compose', ['hooks'], function (hooks) {
     const Compose = {};
