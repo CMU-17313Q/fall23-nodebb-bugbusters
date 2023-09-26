5d533fab53acb397ba72e7aa5b839c0134485db1
Boushrabnd <bushrabendou@gmail.com>
2023-09-26 11:37:13 +0300

    Changing uid to 0 when needed

--- src/posts/edit.js
+++ src/posts/edit.js
@@ -195,6 +195,7 @@ module.exports = function (Posts) {
         const editPostData = {
             content: data.content,
             editor: data.uid,
+            isAnonymous: isAnonymous,
         };
 
         // For posts in scheduled topics, if edited before, use edit timestamp
