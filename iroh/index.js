957a0a78e46785df4296f70f578ae689015c4a45
smubarak7 <smubarak@andrew.cmu.edu>
2023-10-19 03:30:06 +0300

    added iroh as dynamic analysis tool to the project. I also added a file for testing irohs functionality and then added a script to pacakage json using which we can run iroh by command npm run iroh

---
+++ iroh/index.js
@@ -0,0 +1,50 @@
+// Referenced from: https://maierfelix.github.io/Iroh/examples/node.js/index.js
+
+/* eslint-disable */
+const Iroh = require('iroh');
+
+const stage = new Iroh.Stage(`
+function factorial(n) {
+  if (n === 0) return 1;
+  return n * factorial(n - 1);
+};
+factorial(9);
+`);
+
+// function call
+stage.addListener(Iroh.CALL)
+    .on('before', (e) => {
+        const external = e.external ? '#external' : '';
+        console.log(`${' '.repeat(e.indent)}call`, e.name, external, '(', e.arguments, ')');
+        // console.log(e.getSource());
+    })
+    .on('after', (e) => {
+        const external = e.external ? '#external' : '';
+        console.log(`${' '.repeat(e.indent)}call`, e.name, 'end', external, '->', [e.return]);
+        // console.log(e.getSource());
+    });
+
+// function
+stage.addListener(Iroh.FUNCTION)
+    .on('enter', (e) => {
+        const sloppy = e.sloppy ? '#sloppy' : '';
+        if (e.sloppy) {
+            console.log(`${' '.repeat(e.indent)}call`, e.name, sloppy, '(', e.arguments, ')');
+            // console.log(e.getSource());
+        }
+    })
+    .on('leave', (e) => {
+        const sloppy = e.sloppy ? '#sloppy' : '';
+        if (e.sloppy) {
+            console.log(`${' '.repeat(e.indent)}call`, e.name, 'end', sloppy, '->', [void 0]);
+            // console.log(e.getSource());
+        }
+    })
+    .on('return', (e) => {
+        const sloppy = e.sloppy ? '#sloppy' : '';
+        if (e.sloppy) {
+            console.log(`${' '.repeat(e.indent)}call`, e.name, 'end', sloppy, '->', [e.return]);
+            // console.log(e.getSource());
+        }
+    });
+eval(stage.script);
