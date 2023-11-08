fc4059433b1da57595f91efc5cd67f89799fae8d
Boushrabnd <bushrabendou@gmail.com>
2023-11-09 02:09:14 +0300

    Connecting ML to career feature

--- src/controllers/write/career.js
+++ src/controllers/write/career.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// Import the fetch module
+const fetch = require('node-fetch');
+
 const helpers = require('../helpers');
 const user = require('../../user');
 const db = require('../../database');
@@ -9,18 +12,37 @@ const Career = module.exports;
 Career.register = async (req, res) => {
     const userData = req.body;
     try {
-        const userCareerData = {
+        // Prepare the query parameters
+        const queryParams = new URLSearchParams({
             student_id: userData.student_id,
-            major: userData.major,
-            age: userData.age,
             gender: userData.gender,
+            age: userData.age,
+            major: userData.major,
             gpa: userData.gpa,
             extra_curricular: userData.extra_curricular,
             num_programming_languages: userData.num_programming_languages,
-            num_past_internships: userData.num_past_internships,
-        };
+            num_past_internships: userData.num_past_internships
+        }).toString();
 
-        userCareerData.prediction = Math.round(Math.random()); // TODO: Change this line to do call and retrieve actual candidate success prediction from the model instead of using a random number
+        const response = await fetch(`https://s456-wgtqgd7dva-uc.a.run.app/predict?${queryParams}`);
+        
+        // Check if the request was successful
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        
+        const predictionResult = await response.json();
+        
+        // If the response does not contain a 'good_employee' key, handle it appropriately
+        if (typeof predictionResult.good_employee !== 'number') {
+            throw new Error('Invalid prediction response structure');
+        }
+        
+        // Assign the prediction to the user's career data
+        const userCareerData = {
+            ...userData, // spread in the original user data
+            prediction: predictionResult.good_employee
+        };
 
         await user.setCareerData(req.uid, userCareerData);
         db.sortedSetAdd('users:career', req.uid, req.uid);
