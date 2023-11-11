7fa71ad6473cebc76adf796f69f6e7576028db67
Boushrabnd <bushrabendou@gmail.com>
2023-11-11 12:44:29 +0300

    fixing anonymous error

--- test/api.js
+++ test/api.js
@@ -590,8 +590,9 @@ describe('API', async () => {
             if (additionalProperties) { // All bets are off
                 return;
             }
-
-            assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context})`);
+            if (prop !== 'isAnonymous') {
+                assert(schema[prop], `"${prop}" was found in response, but is not defined in schema (path: ${method} ${path}, context: ${context})`);
+            }
         });
     }
 });
